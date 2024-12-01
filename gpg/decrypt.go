2835887df7e6fac077556f9345eaf216fce04af2
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-12-01 13:54:38 +0300

    реализовано шифрование и дешифрование

--- gpg/decrypt.go
+++ gpg/decrypt.go
@@ -1,5 +1,22 @@
 package gpg
 
+import (
+	"bytes"
+	"os"
+	"os/exec"
+)
+
 func Decrypt(data string) (string, error) {
-	return "", nil
+	cmd := exec.Command("gpg", "--decrypt", "--pinentry-mode", "loopback")
+	cmd.Env = append(os.Environ(), "GPG_TTY=/dev/tty")
+	cmd.Stdin = bytes.NewBufferString(data)
+
+	var output bytes.Buffer
+	cmd.Stdout = &output
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	return output.String(), nil
 }
--- gpg/encrypt.go
+++ gpg/encrypt.go
@@ -1,5 +1,23 @@
 package gpg
 
+import (
+	"bytes"
+	"os/exec"
+
+	"github.com/spf13/viper"
+)
+
 func Encrypt(data string) (string, error) {
-	return data, nil
+	r := viper.GetString("gpg_user")
+	cmd := exec.Command("gpg", "--encrypt", "--recipient", r, "--armor")
+	cmd.Stdin = bytes.NewBufferString(data)
+
+	var output bytes.Buffer
+	cmd.Stdout = &output
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	return output.String(), nil
 }
